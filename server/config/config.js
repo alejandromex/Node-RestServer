48cf7bbcd6dd03065296177c89eb4186683b346f
Alejandro Ceballos <alexandro_sin@hotmail.com>
2020-10-26 23:42:30 -0700

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,25 @@
+const { isRegExp } = require("underscore");
+
 // Configuracion del puerto
 process.env.PORT = process.env.PORT || 3000;
 
+//Entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// Base de datos
+
+if(process.env.NODE_ENV == 'dev')
+{
+    urlDB = "mongodb://localhost:27017/cafe";
+}
+else{
+    urlDB =  "mongodb+srv://Alejandromex:Pedro123@cluster0.roire.mongodb.net/cafe";
+}
+
+process.env.URLDB = urlDB;
+
+
+//mongodb+srv://Alejandromex:Pedro123@cluster0.roire.mongodb.net/test
+
+//mongodb+srv://Alejandromex:pedro123@cluster0.roire.mongodb.net/test?authSource=admin&replicaSet=atlas-bwjfzs-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true
--- server/server.js
+++ server/server.js
@@ -1,49 +1,35 @@
 require('./config/config');
 const express = require('express')
-const app = express();
+const mongoose = require('mongoose');
 const bodyparser = require('body-parser');
+
+const app = express();
+
+
 
 //Middlewares
 app.use(bodyparser.urlencoded({extended: false}));
 app.use(bodyparser.json());
  
-app.get('/usuario', function (req, res) {
-  res.send('get Usuario')
+
+app.use( require('./routes/usuario') );
+
+ mongoose.connect(process.env.URLDB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+},(err)=>{
+    if(err)
+    {
+        console.log("Error");
+    }
+    else{
+        console.log("Conexion a la base de datos correcta");
+    }
 });
 
 
-app.post('/usuario',(req,res)=>{
-    
-    let body = req.body;
-    
-    let nombre = req.body.nombre;
-    let edad = req.body.edad;
-
-    if(body.nombre == undefined || body.edad == undefined)
-    {
-        res.status(400).json({
-            ok: false,
-            message: "The name and age is required"
-        });
-    }
-    else
-    {
-        res.send({
-            nombre,
-            edad,
-        });
-    }
-
-});
-
-app.put("/usuario/:id",(req,res)=>{
-    let id = req.params.id;
-    res.send({
-        id,
-        message: "Funcion put para actualizar usuario",
-        code: 200
-    });
-})
  
 app.listen(process.env.PORT,()=>{
     console.log(`Conexion establecida en el puerto 3000`);
